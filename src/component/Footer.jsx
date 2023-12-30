796df8435eb762d616f1a77d5bbc8ac4146a690d
Anirudha Das <anirudhadas2701@gmail.com>
2023-12-30 19:05:34 +0530

    h

---
+++ src/component/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='footer' >
+                <div className='sec1'>
+                    <ul>
+                        <li>
+                            <p>ABOUT</p>
+                        </li>
+                        <li>
+                            <p>HELP</p>
+                        </li>
+                        <li>
+                            <p>CONSUMER POLICY</p>
+                        </li>
+                        <li>
+                            <p>SOCIAL</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className='sec2'>
+
+                </div>
+                <div className='sec3'>
+
+                </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
